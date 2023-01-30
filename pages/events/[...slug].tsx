0c42d173898284f0bd8d016349670fc30bb18a28
howooking <junsgk@gmail.com>
2023-01-30 11:45:47 +0900

    dummy added

--- pages/events/[...slug].tsx
+++ pages/events/[...slug].tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 
-export default function FilterPage() {
+export default function FilteredEvent() {
   const router = useRouter();
   console.log(router.query);
   return <div>filters evety events</div>;
